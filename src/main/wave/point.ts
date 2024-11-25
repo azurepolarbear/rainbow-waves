352af33e09f2237990605056a040b3489a3ca656
azurepolarbear <azurepolarbear@users.noreply.github.com>
2024-11-24 19:43:48 -0600

    Add point class. All the classes got deleted somehow. I don't know what happened.

--- src/main/wave/point.ts
+++ src/main/wave/point.ts
@@ -20,3 +20,75 @@
  * See OUTPUT-LICENSE or go to https://creativecommons.org/licenses/by-nc-nd/4.0/
  * for full license details.
  */
+
+import P5Lib from 'p5';
+
+import { CanvasRedrawListener, Color, Coordinate, CoordinateMode, Point as PointShape } from '@batpb/genart';
+
+export interface PointConfig {
+    readonly base: P5Lib.Vector | { x: number; y: number; };
+    readonly coordinateMode: CoordinateMode;
+    readonly amplitude: number;
+    readonly theta: number;
+    readonly deltaTheta: number;
+    readonly color: Color;
+}
+
+export class Point implements CanvasRedrawListener {
+    readonly #point: PointShape;
+    readonly #base: Coordinate = new Coordinate();
+    readonly #deltaTheta: number;
+
+    #theta: number;
+    #amplitude: number;
+
+    public constructor(config: PointConfig) {
+        if (config.base instanceof P5Lib.Vector) {
+            this.#base.setPosition(config.base, config.coordinateMode);
+        } else {
+            this.#base.setX(config.base.x, config.coordinateMode);
+            this.#base.setY(config.base.y, config.coordinateMode);
+        }
+
+        this.#amplitude = config.amplitude;
+        this.#theta = config.theta;
+        this.#deltaTheta = config.deltaTheta;
+        this.#point = new PointShape({ coordinateMode: CoordinateMode.CANVAS });
+        this.#point.style.stroke = config.color;
+        this.#point.style.strokeMultiplier = 4;
+        this.updatePosition();
+    }
+
+    public draw(): void {
+        this.#point.draw();
+        this.#update();
+    }
+
+    public canvasRedraw(): void {
+        this.#base.remap();
+        this.#point.canvasRedraw();
+    }
+
+    public set amplitude(amplitude: number) {
+        this.#amplitude = amplitude;
+    }
+
+    public get base(): Coordinate {
+        return this.#base;
+    }
+
+    public updatePosition(): void {
+        const mode: CoordinateMode = CoordinateMode.CANVAS;
+        this.#point.setX(this.#base.getX(mode), mode);
+        this.#point.setY(this.#calculateY(), mode);
+    }
+
+    #update(): void {
+        this.#theta = (this.#theta + this.#deltaTheta) % (Math.PI * 2);
+        this.updatePosition();
+    }
+
+    #calculateY(): number {
+        return (this.#base.getY(CoordinateMode.CANVAS) + (Math.sin(this.#theta) * this.#amplitude));
+    }
+}
